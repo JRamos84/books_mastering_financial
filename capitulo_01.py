be826558433680bb7e05341691687c87ffc3b196
Jose Ramos <joseph0001@gmail.com>
2024-08-02 16:38:52 -0300

    capitulo 3

--- capitulo_01.py
+++ capitulo_01.py
@@ -19,19 +19,12 @@ if not mt5.symbol_select(symbol, True):
     sys.exit()
 
 # Definición de los timeframes
-frame_M15 = mt5.TIMEFRAME_M15
-frame_M30 = mt5.TIMEFRAME_M30
 frame_H1 = mt5.TIMEFRAME_H1
-frame_H4 = mt5.TIMEFRAME_H4
-frame_D1 = mt5.TIMEFRAME_D1
-frame_W1 = mt5.TIMEFRAME_W1
-frame_M1 = mt5.TIMEFRAME_MN1
-frame_M5 = mt5.TIMEFRAME_M5
 
 def get_quotes(time_frame, year=2024, month=1, day=1, asset='EURUSD'):
     timezone = pytz.timezone('Etc/UTC')
     time_from = datetime.datetime(2023, 1, 10, tzinfo=timezone)
-    time_to = datetime.datetime(2024, 2,10,  tzinfo=timezone) # Fecha actual en UTC
+    time_to = datetime.datetime(2024, 2, 10, tzinfo=timezone) # Fecha actual en UTC
     print('adui',datetime.datetime.now(timezone))
     print('ui',time_to)
     print(f"Obteniendo datos desde {time_from} hasta {time_to} para el activo {asset} en el marco de tiempo {time_frame}")
@@ -49,15 +42,9 @@ def mass_import(asset, time_frame):
     data = None
     if time_frame == 'H1':
         data = get_quotes(frame_H1, 2013, 1, 1, asset=asset)
-    elif time_frame == 'D1':
-        data = get_quotes(frame_D1, 2000, 1, 1, asset=asset)
-    elif time_frame == 'M5':
-        data = get_quotes(frame_M5, 2000, 1, 1, asset=asset)
-
     if data is not None:
-        data = data.iloc[:, 1:5]  # Seleccionar columnas relevantes
+        data = data.iloc[:, 1:5]  # Seleccionar columnas relevantes (open, high, low, close)
         data = data.round(decimals=5)
-
     return data
 
 def add_column(data, times):
@@ -66,104 +53,93 @@ def add_column(data, times):
         data = np.append(data, new, axis=1)
     return data
 
-def delete_column(data, index, times):
-    for i in range(times):
-        data = np.delete(data, index, axis=1)
-    return data
-
-def add_row(data, times):
-    for i in range(times):
-        columns = np.shape(data)[1]
-        new = np.zeros((1, columns), dtype=float)
-        data = np.append(data, new, axis=0)
-    return data
-
-def delete_row(data, number):
-    return data[number:,]
-
-def rounding(data, how_far):
-    return data.round(decimals=how_far)
-
 def signal(data):
-    data = add_column(data, 5)
+    data = add_column(data, 2)
     for i in range(len(data)):
         try:
+            # Bullish Alpha
             if data[i, 2] < data[i - 5, 2] and data[i, 2] < data[i - 13, 2] and data[i, 2] > data[i - 21, 2] and data[i, 3] > data[i - 1, 3] and data[i, 4] == 0:
                 data[i + 1, 4] = 1
+            # Bearish Alpha
             elif data[i, 1] > data[i - 5, 1] and data[i, 1] > data[i - 13, 1] and data[i, 1] < data[i - 21, 1] and data[i, 3] < data[i - 1, 3] and data[i, 5] == 0:
                 data[i + 1, 5] = -1
         except IndexError:
             pass
     return data
 
-def ohlc_plot_bars(data, window):
+def ohlc_plot_bars(data, window, ax):
     sample = data[-window:, :]
     for i in range(len(sample)):
-        plt.vlines(x=i, ymin=sample[i, 2], ymax=sample[i, 1], color='black', linewidth=1)
+        ax.vlines(x=i, ymin=sample[i, 2], ymax=sample[i, 1], color='black', linewidth=1)
         if sample[i, 3] > sample[i, 0]:
-            plt.vlines(x=i, ymin=sample[i, 0], ymax=sample[i, 3], color='black', linewidth=1)
+            ax.vlines(x=i, ymin=sample[i, 0], ymax=sample[i, 3], color='black', linewidth=1)
         elif sample[i, 3] < sample[i, 0]:
-            plt.vlines(x=i, ymin=sample[i, 3], ymax=sample[i, 0], color='black', linewidth=1)
+            ax.vlines(x=i, ymin=sample[i, 3], ymax=sample[i, 0], color='black', linewidth=1)
         else:
-            plt.vlines(x=i, ymin=sample[i, 3], ymax=sample[i, 0] + 0.0003, color='black', linewidth=1)
-    plt.grid()
-    plt.show()
-    
+            ax.vlines(x=i, ymin=sample[i, 3], ymax=sample[i, 0] + 0.0003, color='black', linewidth=1)
+    ax.grid()
+
 def signal_chart(data, position, buy_column, sell_column, window=500):
-    # Utiliza iloc para cortar el DataFrame
- 
     sample = data[-window:,]
-    
     print("Shape of sample:", sample.shape)
     
     fig, ax = plt.subplots(figsize=(10, 5))
     
-    ohlc_plot_bars(data, window)
+    ohlc_plot_bars(data, window, ax)
     
-    # Verifica los índices de columna
     num_columns = sample.shape[1]
     print(f"Number of columns in sample: {num_columns}")
     
     for i in range(len(sample)):
-        if  sample[i, buy_column] == 1:
+        if sample[i, buy_column] == 1:
+           # print(f'Compra en índice {i}')
             x = i
             y = sample[i, position]
             ax.annotate('', xy=(x, y), xytext=(x, y),
-                        arrowprops=dict(width=9, headlength=11, headwidth=11, facecolor='green', edgecolor='green'))
+                        arrowprops=dict(width=1, headlength=10, headwidth=10, facecolor='green', edgecolor='green'))
         if sample[i, sell_column] == -1:
+            #print(f'Venta en índice {i}')
             x = i
             y = sample[i, position]
             ax.annotate('', xy=(x, y), xytext=(x, y),
-                        arrowprops=dict(width=9, headlength=11, headwidth=11, facecolor='red', edgecolor='red'))
+                        arrowprops=dict(width=1, headlength=10, headwidth=10, facecolor='red', edgecolor='red'))
  
     plt.show()
-    
+
+
+def performance(data, open_price, buy_column, sell_column, long_result_col, short_result_col):
+    # Itera sobre las filas para calcular el rendimiento de las posiciones largas
+    for i in range(len(data)):
+        try:
+            if data[i, buy_column] == 1:
+                for a in range(i+1, min(i+1000, len(data))):
+                    if data[a, buy_column] == 1 or data[a, sell_column] == -1:
+                        data[a, long_result_col] = data[a, open_price] - data[i, open_price]
+                        break
+        except IndexError:
+            pass
+
+    # Itera sobre las filas para calcular el rendimiento de las posiciones cortas
+    for i in range(len(data)):
+        try:
+            if data[i, sell_column] == -1:
+                for a in range(i+1, min(i+1000, len(data))):
+                    if data[a, buy_column] == 1 or data[a, sell_column] == -1:
+                        data[a, short_result_col] = data[i, open_price] - data[a, open_price]
+                        break
+        except IndexError:
+            pass
+                    
+
 
 # Ejemplo de uso
-
-# Paso 1: Importar datos
+horizon = 'H1'
 asset = 'USDJPY'
-time_frame = 'H1'
-data = mass_import(asset, time_frame)
+mydata = mass_import(asset, horizon)
+mydata = signal(mydata)
 
-if data is not None:
-    # Convertir DataFrame a numpy array
-    data_array = data.to_numpy()
+# Verificar si las señales se han generado correctamente
+print(mydata[-50:])  # Imprimir las últimas filas para verificar las señales
 
-    # Paso 2: Generar señales
-    data = signal(data_array)
-
-    # Paso 3: Plotear datos OHLC con señales
-   # ohlc_plot_bars(data_with_signals, window=100)
-    ##marcar Posicin
-    
-    signal_chart(data,0, 4, 5, window = 500)
-    
-else:
-    print("No se pudieron obtener datos para el análisis.")
-
-# Finalizar conexión a MetaTrader 5
-mt5.shutdown()
-
-
+signal_chart(mydata, 3, 4, 5, window=500)  # Cambiar `position` a 3 para usar la columna 'close'
 
